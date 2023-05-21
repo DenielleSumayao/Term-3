a7cc806a86c323bbb7261427727c734c15c00df2
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-05-22 00:22:36 +0800

    Update BiographyData.java

--- [DRAFT] Portfolio/BiographyData.java
+++ [DRAFT] Portfolio/BiographyData.java
@@ -1,12 +1,12 @@
 package SumayaoPortfolio;
 
 public class BiographyData extends Database{
-	String name = "Luke Denielle L. Sumayao";
-	String age = "19";
-	String birthday = "October 15, 2003";
-	String eyes = "black";
-	String height = "5ft 9in";
-	String weight = "52kg";
-	String nationality = "Filipino";
-	String motto = "You can't do everything perfectly; but always strive to be better.";
+	String Name = "Luke Denielle L. Sumayao";
+	String Age = "19";
+	String Birthday = "October 15, 2003";
+	String Eyes = "black";
+	String Height = "5ft 9in";
+	String Weight = "52kg";
+	String Nationality = "Filipino";
+	String Motto = "You can't do everything perfectly; but always strive to be better.";
 }
