a882d24f56b00a2dea2a58dc5ed41bbb80e3f0e0
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-05-21 23:40:51 +0800

    Create FacultyDesignation.java

---
+++ Inheritance/Multi-level Inheritance/FacultyDesignation.java
@@ -0,0 +1,9 @@
+public class FacultyDesignation extends ProgrammingTeacher{
+	String faculty = "Faculty B";
+	String floor = "2nd Floor";
+
+	void facultyDesignation() {
+		System.out.println("Faculty Designation");
+	}
+}
+
