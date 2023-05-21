7b7ba111f32d2e925c981c4243d0395763654f3e
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-05-21 23:40:23 +0800

    Create ProgrammingTeacher.java

---
+++ Inheritance/Multi-level Inheritance/ProgrammingTeacher.java
@@ -0,0 +1,10 @@
+public class ProgrammingTeacher extends Teacher{
+	String subject = "Programming";
+	String department = "Computer Science";
+	String teacherName = "Arnold Shortman";
+	
+	void action() {
+		System.out.println("Program");
+	}
+}
+
