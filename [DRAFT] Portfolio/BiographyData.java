fe54fe0292034b8f5e7a4778f3eb829323bff467
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-05-22 00:20:01 +0800

    Create BiographyData.java

---
+++ [DRAFT] Portfolio/BiographyData.java
@@ -0,0 +1,12 @@
+package SumayaoPortfolio;
+
+public class BiographyData extends Database{
+	String name = "Luke Denielle L. Sumayao";
+	String age = "19";
+	String birthday = "October 15, 2003";
+	String eyes = "black";
+	String height = "5ft 9in";
+	String weight = "52kg";
+	String nationality = "Filipino";
+	String motto = "You can't do everything perfectly; but always strive to be better.";
+}
