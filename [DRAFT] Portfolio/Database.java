5379dd1f188b65282210125cb3a03b1a25873134
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-05-22 00:32:47 +0800

    Delete Database.java

--- [DRAFT] Portfolio/Database.java
+++
@@ -1,9 +0,0 @@
-package SumayaoPortfolio;
-
-public class Database {
-	public static void main(String args[]) {
-	    BiographyData bio = new BiographyData();
-	    
-	    System.out.println(bio.Name);
-}
-}
