0150ea098595ad5e9172f8f030f7f15226d92537
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-05-25 11:35:28 +0800

    Create Vehicle.java

---
+++ Inheritance/Vehicle/Vehicle.java
@@ -0,0 +1,13 @@
+package InheritanceOverriding;
+
+class Vehicle {
+	
+	String speed = "180";
+	String color = "Silver Grey";
+	String price = "425,000.00";
+	
+
+	public void stop() {
+		System.out.println("Vehicle has stopped.");
+	}
+}
