cfd767afc0b6e92b5fe3fc138afe794b7fc6b9db
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-06-06 21:27:57 +0800

    Create Inheritance2.java

---
+++ Final Portfolio/Inheritance2.java
@@ -0,0 +1,12 @@
+package Portfolio;
+
+public class Inheritance2 extends Inheritance{
+	String Education1 = "San Benildo Integrated School";
+	String Education2 = "St. Mary's College of Baliuag";
+	String Education3 = "National University of the Philippines";
+	String Education4 = "Foundation (Baliuag) Inc.";
+	String College = "College (2023)";
+	String SHS = "Senior High School (2022)";
+	String JHS = "Junior High School (2020)";
+	String Elem = "Elementary (2016)";
+}
