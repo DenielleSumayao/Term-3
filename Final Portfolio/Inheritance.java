2db23680713ea6761e231694967551f8b95b7934
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-06-06 21:27:43 +0800

    Create Inheritance.java

---
+++ Final Portfolio/Inheritance.java
@@ -0,0 +1,17 @@
+package Portfolio;
+
+public class Inheritance extends Database{
+	String Play1 = "St. Bro. Benilde (2016)";
+	String Play2 = "New Yorker in Tondo (2017)";
+	String Play3 = "Tanglaw (2018)";
+	String Play4 = "Panuluyan: Emmanuel (2018)";
+	String Play5 = "Anluwagi (2019)";
+	String Play6 = "Eukaristiya (2020)";
+	String Role1 = "St. Benilde's student";
+	String Role2 = "a young protagonist";
+	String Role3 = "Donya Victorina's helper";
+	String Role4 = "an angel from the real world";
+	String Role5 = "lead dancer";
+	String Role6 = "St. Matthew";
+}
+
