25320b853b3375fe50da83e36be52680e8376d10
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-06-06 21:26:02 +0800

    Create Database.java

---
+++ Final Portfolio/Database.java
@@ -0,0 +1,13 @@
+package Portfolio;
+
+public class Database {
+	String AboutMe1 = "English";
+	String AboutMe2 = "Filipino";
+	String Hobby1 = "Playing Video Games";
+	String Hobby2 = "Logo Designing";
+	String Hobby3 = "Performing";
+	String Program = "BSIT with Specialization in Multimedia Arts & Animation";
+	String Artwork1 = "\"Leia\" (2020)";
+	String Artwork2 = "\"Our Friend, Totoro\" (2021)";
+	String Artwork3 = "\"Jane\" (2022)";
+}
